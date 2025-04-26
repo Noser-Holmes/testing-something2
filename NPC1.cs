66ab01569e336f26ecc77e996e8355e2c5ea9325
Noser-Holmes <7oda7oda804@gmail.com>
2025-04-26 16:33:48 +0300

    Add files via upload

---
+++ NPC1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC1 : MonoBehaviour
+{
+
+   int health = 5 ;
+  int level = 6 ;
+  float speed = 1.5f ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 2
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 3
+    }
+
+}
